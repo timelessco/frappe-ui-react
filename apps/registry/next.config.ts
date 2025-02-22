ab147024d1bb85b9ed5a9b4675f02e8b0f78d981
Naveen MC <8493007+mcnaveen@users.noreply.github.com>
2025-02-22 19:44:31 +0530

    feat(docs): ✨ add docs skeleton
    - Introduce new documentation skeleton
    - Enhance the layout and navigation for better user experience
    - Update Next.js configuration to support MDX files
    - Add Tailwind CSS typography plugin for improved styling

--- apps/registry/next.config.ts
+++ apps/registry/next.config.ts
@@ -1,10 +1,11 @@
 import { type NextConfig } from "next";
+import withMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
 	outputFileTracingIncludes: {
 		registry: ["./registry/**/*"],
 	},
-	/* config options here */
+	pageExtensions: ["ts", "tsx", "mdx"],
 };
 
-export default nextConfig;
+export default withMDX()(nextConfig);
