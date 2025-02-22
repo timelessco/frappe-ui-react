764ca5ddcbd18e49a907a1fc2a7d204a340a16fc
Navin Moorthy <navin007.a@gmail.com>
2025-02-22 22:42:22 +0530

    feat(mode-toggle): ✨ add mode toggle with dynamic icons
    - Implement mode toggle functionality with light/dark theme icons
    - Refactor button to use a dedicated ModeToggleIcon component
    - Enhance user experience by showing appropriate icons based on theme

--- apps/registry/components/mode-toggle.tsx
+++ apps/registry/components/mode-toggle.tsx
@@ -11,23 +11,15 @@ import {
 	DropdownMenuItem,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { type IconProps } from "@/components/ui/icon";
 
 export function ModeToggle(props: React.ComponentProps<typeof Button>) {
-	const { theme, setTheme } = useTheme();
+	const { setTheme } = useTheme();
 
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<Button
-					iconOnly={
-						theme === "light" ? (
-							<Sun className="inline-block size-[1em] shrink-0 align-middle leading-[1em]" />
-						) : (
-							<Moon className="inline-block size-[1em] shrink-0 align-middle leading-[1em]" />
-						)
-					}
-					{...props}
-				/>
+				<Button iconOnly={<ModeToggleIcon />} {...props} />
 			</DropdownMenuTrigger>
 			<DropdownMenuContent align="end">
 				<DropdownMenuItem onClick={() => setTheme("light")}>
@@ -43,3 +35,26 @@ export function ModeToggle(props: React.ComponentProps<typeof Button>) {
 		</DropdownMenu>
 	);
 }
+
+const ModeToggleIcon = (props: IconProps) => {
+	const { theme } = useTheme();
+
+	return (
+		<>
+			<Sun
+				suppressHydrationWarning
+				className={`inline-block size-[1em] shrink-0 align-middle leading-[1em] ${
+					theme === "dark" ? "hidden" : "block"
+				}`}
+				{...props}
+			/>
+			<Moon
+				suppressHydrationWarning
+				className={`inline-block size-[1em] shrink-0 align-middle leading-[1em] ${
+					theme === "dark" ? "block" : "hidden"
+				}`}
+				{...props}
+			/>
+		</>
+	);
+};
