52fa8064995a96c49a83af5f08f993f1f1a58732
padmanathan10 <58564318+padmanathan10@users.noreply.github.com>
2025-02-23 13:47:52 +0530

    feat(lms): ✨ create lms layout for web (#81)

---
+++ apps/registry/icons/line/search.tsx
@@ -0,0 +1,12 @@
+import { Icon, type IconProps } from "@/components/ui/icon";
+
+export const SearchIcon = (props: IconProps) => (
+	<Icon fill="none" viewBox="0 0 16 16" {...props}>
+		<path
+			fill="currentColor"
+			fillRule="evenodd"
+			d="M12.58 7.526a5.001 5.001 0 1 1-10.002 0 5.001 5.001 0 0 1 10.002 0Zm-1.184 4.63a6.001 6.001 0 1 1 .716-.698l1.062 1.063.63.629.314.314.157.158a.5.5 0 1 1-.707.707l-.157-.158-.315-.314-.629-.63-1.07-1.07Z"
+			clipRule="evenodd"
+		/>
+	</Icon>
+);
---
+++ apps/registry/icons/line/star.tsx
@@ -0,0 +1,10 @@
+import { Icon, type IconProps } from "@/components/ui/icon";
+
+export const StarIcon = (props: IconProps) => (
+	<Icon fill="none" viewBox="0 0 16 16" {...props}>
+		<path
+			fill="#DB7706"
+			d="M7.357 2.068a.75.75 0 0 1 1.286 0l1.78 2.958 3.363.779a.75.75 0 0 1 .397 1.222L11.92 9.634l.299 3.44a.75.75 0 0 1-1.04.755L8 12.482l-3.179 1.347a.75.75 0 0 1-1.04-.756l.299-3.44-2.263-2.606a.75.75 0 0 1 .397-1.222l3.363-.78 1.78-2.957Z"
+		/>
+	</Icon>
+);
